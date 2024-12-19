380dd381d51a613e52e1889b103ceddfefc67c1a
Shadow8928 <92939107+Shadow8928@users.noreply.github.com>
2024-12-19 17:47:45 -0600

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,13 @@
+import main
+while True:
+	text = input('basic > ')
+	if text.strip() == "": continue
+	result, error = main.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
